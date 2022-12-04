bcd2aa8c593f31ae592444bdb0d4fa6318bce9dc
Andrey Moises <andreymoises.garcia@gmail.com>
2022-12-03 19:44:22 -0600

    Primera version - solo menu

---
+++ app/src/main/java/com/example/proyecto_final/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.proyecto_final;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
