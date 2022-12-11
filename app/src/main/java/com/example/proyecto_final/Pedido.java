18da1c33ef98364f234485ae89dce8ba7392fd50
Andrey Moises <andreymoises.garcia@gmail.com>
2022-12-10 23:39:18 -0600

    Los 2 puntos adicionales completados

--- app/src/main/java/com/example/proyecto_final/Pedido.java
+++ app/src/main/java/com/example/proyecto_final/Pedido.java
@@ -2,13 +2,46 @@ package com.example.proyecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Pedido extends AppCompatActivity {
+
+    EditText de;
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+
+        Intent GO = getIntent();
+
+        de = (EditText) findViewById(R.id.de);
+        boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sde = de.getText().toString().trim();
+
+                despliegueRuta(sde);
+
+            }
+        });
+    }
+
+    private void despliegueRuta(String sde) {
+        Uri uri = Uri.parse("http://www.google.co.in/maps/dir/" + "Unitec Atizapan" + "/" + sde);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        intent.setPackage("com.google.android.apps.maps");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
     }
 }
+
