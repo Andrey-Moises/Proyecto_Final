465748d0af0ebc16eb5e19c3c887bc10fcedf019
Andrey Moises <andreymoises.garcia@gmail.com>
2022-12-10 21:22:36 -0600

    Encuentranos y agendar funcionando

--- app/src/main/java/com/example/proyecto_final/Agendar.java
+++ app/src/main/java/com/example/proyecto_final/Agendar.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
 
 public class Agendar extends AppCompatActivity {
 
@@ -13,6 +16,22 @@ public class Agendar extends AppCompatActivity {
         setContentView(R.layout.activity_agendar);
 
         Intent GO = getIntent();
+        Button boton;
+
+        boton = (Button) findViewById(R.id.boton);
+
+        boton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent= new Intent(Intent.ACTION_INSERT);
+                intent.setData(CalendarContract.Events.CONTENT_URI);
+                startActivity(intent);
+            }
+
+        });
+
 
     }
 }
--- app/src/main/java/com/example/proyecto_final/Encuentranos.java
+++ app/src/main/java/com/example/proyecto_final/Encuentranos.java
@@ -5,14 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class Encuentranos extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Encuentranos extends AppCompatActivity implements OnMapReadyCallback
+{
+
+    GoogleMap mapa;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encuentranos);
 
         Intent GO = getIntent();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
+
+        mapa = googleMap;
+        LatLng unitec = new LatLng(19.545956791791628,-99.2408773456682);
+        mapa.addMarker(new MarkerOptions().position(unitec).title("Restaurante Carmelias"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(unitec));
+        mapa.animateCamera(CameraUpdateFactory.zoomTo(15));
 
     }
 }
