5f9852c95e7b221b328ac717eba37c092ba9d8e6
Andrey Moises <andreymoises.garcia@gmail.com>
2022-12-10 22:12:42 -0600

    Puntos basicos cubiertos. Faltan puntos extras

--- app/src/main/java/com/example/proyecto_final/Video.java
+++ app/src/main/java/com/example/proyecto_final/Video.java
@@ -3,9 +3,13 @@ package com.example.proyecto_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Video extends AppCompatActivity {
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,10 @@ public class Video extends AppCompatActivity {
         setContentView(R.layout.activity_video);
 
         Intent GO = getIntent();
+
+        videoView = findViewById(R.id.video);
+        videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.sample1));
+        videoView.start();
 
     }
 }
